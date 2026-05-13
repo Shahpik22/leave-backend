ada184142593789513fac36fd4f0fed547edc33b
Shahpik22 <cdcshah@gmail.com>
2026-05-13 19:57:56 +0800

    Update environment

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log('MongoDB Connected'))
 .catch(err => console.log(err));
 
